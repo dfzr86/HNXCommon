ab9d6e15ede3db89bb3eaa67fe2733a1d1c01206
__zimu <dfzr86>
2016-05-27 12:53:23 +0800

    修改了注释

--- INSCache.h
+++ INSCache.h
@@ -9,15 +9,15 @@
 #import <Foundation/Foundation.h>
 
 @interface INSCache : NSObject
-
+//缓存当前系统版本
 + (void)cacheAppVersion;
-
+//获取缓存的系统版本
 + (NSString *)cachedAppVersion;
-
+//获取当前系统版本
 + (NSString *)currentAppVersion;
-
+//app是否是第一次安装
 + (BOOL)appFirstInstall;
-
+//app是否升级了版本
 + (BOOL)appUpgrade;
 
 @end
--- INSCommon.h
+++ INSCommon.h
@@ -9,13 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface INSCommon : NSObject
-
+//获取当前时间
 + (NSString *)ins_currentTime;
-
+//监控网络状态
 + (NSString *)ins_network_status;
-
+//获取指定长度的随机字符串
 + (NSString *)ins_nonce_by_length:(NSInteger)length;
-
+//获取手机的基本信息
 + (NSDictionary *)ins_phone_info;
 
 @end
